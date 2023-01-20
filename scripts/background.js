07c54135c10873904873f425effa671170198a3e
gwongibeom <k201gun@kakao.com>
2023-01-20 20:46:29 +0900

    diff background change time

--- scripts/background.js
+++ scripts/background.js
@@ -1,14 +1,14 @@
 $(document).ready(() => {
-    let fRandomNumber = Math.floor(Math.random() * 12) + 1;
-    $('.bg').addClass('bg' + fRandomNumber);
+    let fRandomNumber = Math.floor(Math.random() * 12) + 1
+    $(".bg").addClass("bg" + fRandomNumber)
 
     setInterval(() => {
-        let randomNumber = Math.floor(Math.random() * 12) + 1;
-        $('.bg').removeClass('bg' + fRandomNumber);
-        $('.bg').addClass('bg' + randomNumber);
-        console.log(randomNumber);
+        let randomNumber = Math.floor(Math.random() * 12) + 1
+        $(".bg").removeClass("bg" + fRandomNumber)
+        $(".bg").addClass("bg" + randomNumber)
+        console.log(randomNumber)
         setTimeout(() => {
-            $('.bg').removeClass('bg' + randomNumber);
-        }, 30000);
-    }, 30000);
-});
+            $(".bg").removeClass("bg" + randomNumber)
+        }, 20000)
+    }, 20000)
+})
